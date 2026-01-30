1bdcba18e275e4a6a6c9006c03bc6dcd3b9708ce
Ettore24 <ettore.castro.quichimbo@edu.immaginazioneelavoro.it>
2026-01-30 10:19:47 +0100

    .

---
+++ script.js
@@ -0,0 +1,22 @@
+function tasto(numero) {
+    let a = document.getElementById("numeri");
+    a.innerText = a.innerText + numero;
+}
+
+function cancella() {
+    document.getElementById("numeri").innerText = "";
+}
+
+let primoNum = 0;
+function somma() {
+    // trasformo il primo numero in numero e lo salvo 
+    primoNum = Number(document.getElementById("numeri").innerText);
+    console.log(primoNum);
+    document.getElementById("numeri").innerText = "";
+}
+
+function uguale() {
+    let secondoNum = Number(document.getElementById("numeri").innerText);
+    let risultato = primoNum + secondoNum;
+    document.getElementById("numeri").innerText = risultato;
+}
